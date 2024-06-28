46d49e8de0f03b5a2e812b8928d3835b66e39a20
Mateusz <156026954+maqcode7@users.noreply.github.com>
2024-06-28 23:35:18 +0200

    Add files via upload
    Basic script for finding target as a sum of two numbers in an array
    Input -> numbers separated by space, then target
    Output -> index of two sum digits

---
+++ FindingSum.py
@@ -0,0 +1,7 @@
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+target = int(input("Enter target: "))
+a = len(nums)
+for x in range(a):
+    for y in range(x+1, a):
+        if (nums[x]+nums[y]) == target:
+            print ([x,y])
